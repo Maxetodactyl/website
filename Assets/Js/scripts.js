7b98a58762e4d61749b69d8126bdad3dc77f69b2
Maxetow <149953990+Maxetow@users.noreply.github.com>
2025-01-02 22:55:57 +0100

    js updt

--- Assets/Js/scripts.js
+++ Assets/Js/scripts.js
@@ -1,6 +1,6 @@
 // Load external content (e.g., navbar, footer)
 function loadContent(containerId, fileName, callback) {
-    fetch(`/components/${fileName}`)  // Corrected path to the component folder
+    fetch(`/Components/${fileName}`)  // Corrected path to the component folder  // Corrected path to the component folder
         .then(response => {
             if (!response.ok) {
                 throw new Error(`Failed to fetch ${fileName}: ${response.statusText}`);
